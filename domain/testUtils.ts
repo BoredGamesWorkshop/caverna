8eee2a5d9a2a5a28f5ed1d2a37c1d2b2cc718955
Menghan Yang <menghan.yang@360learning.com>
2020-11-04 16:00:51 +0100

    Extract test utils for actions

---
+++ domain/testUtils.ts
@@ -0,0 +1,47 @@
+import { EntityType, isMutationOfType, Mutation } from "./Mutation";
+import { expect } from "chai";
+import { buildInitialGame } from "./initializeGame";
+import { Action, ActionSpace } from "./ActionSpace";
+import { Dwarf } from "./Player";
+
+export function shouldPlaceDwarf(action: Action) {
+    describe("should place a dwarf", () => {
+        it("should use a dwarf", () => {
+            const { game, player } = buildBaseObjects();
+
+            const mutations = action(game, player.id);
+
+            expectMutationsOfType(mutations, Dwarf).toVerifyOnce(
+                (mutation) => mutation.diff.isAvailable === false
+            );
+        });
+
+        it("should use an action space", () => {
+            const { game, player } = buildBaseObjects();
+
+            const mutations = action(game, player.id);
+
+            expectMutationsOfType(mutations, ActionSpace).toVerifyOnce(
+                (mutation) => !!mutation.diff.dwarf
+            );
+        });
+    });
+}
+
+export function buildBaseObjects() {
+    const game = buildInitialGame();
+    const firstPlayer = Array.from(game.players.values())[0];
+    return { game: game, player: firstPlayer };
+}
+
+export function expectMutationsOfType<T extends EntityType>(
+    mutations: Mutation<EntityType>[],
+    classType: { new (...arg: any): T }
+) {
+    const mutationsT = mutations.filter(isMutationOfType(classType));
+    return {
+        toVerifyOnce: (check: (mutation: Mutation<T>) => boolean) => {
+            expect(mutationsT.filter((mutation) => check(mutation))).to.have.lengthOf(1);
+        },
+    };
+}
