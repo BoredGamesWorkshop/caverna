106c68c9812bd74f8a80ba564d9bc6b4f887377e
Maxence Dallongeville <maxence.dallongeville@360learning.com>
2020-11-05 14:58:43 +0100

    Shorten the comment

--- domain/entity/Mutation.ts
+++ domain/entity/Mutation.ts
@@ -8,26 +8,13 @@ type Mutation<T> = {
     diff: Partial<T>;
 };
 
-/*
-  Note TS:
-
-  Mutation<EntityPlayer> was not satisfying because it doesn't narrow down the type of T to Player, Dwarf & co. It checks
-  that both `original` and `diff` are respectively based from any EntityType but not that they are based from the same
-  entity subtype
-
-  We would rather be interested in "Mutation<Player> | Mutation<Dwarf> | ..." so we created DiscriminateUnion<EntityType>
-  (that is equivalent to that) to replace Mutation<EntityType>
-
-  The problem is that in the type guard "mutation is Mutation<T>", the type predicate's type (Mutation<T>)
-  must be assignable to its parameter's type (Mutation<Player> | Mutation<Dwarf> | ...). And it doesn't work when T is
-  equal to EntityType as Mutation<EntityType> is not included in (Mutation<Player> | Mutation<Dwarf> | ...) for the
-  reasons explained previously.
-
-  To fix that, we want to explicitly remove the case Mutation<EntityType>. We achieved that by creating a new type
-  EntityMutation<T> that is an intersection of Mutation<T> and DiscriminateUnion<EntityType>. And that way, everything
-  works ;)
- */
+// DiscriminateUnion<EntityType> is equivalent to Mutation<Player> | Mutation<Dwarf> | ...
+// It doesn't include the type Mutation<EntityType> that allows "original" and "diff" attributes of different EntityType
 type DiscriminateUnion<T> = T extends any ? Mutation<T> : never;
+
+// The problem is that Mutation<T> is not included in DiscriminateUnion<EntityType> because of Mutation<EntityType>
+// and so we can't use the type guard below to narrow the type of Mutation<T> (see https://github.com/microsoft/TypeScript/issues/24935)
+// To fix that, we create a new type with an intersection to exclude Mutation<EntityType>
 export type EntityMutation<T> = DiscriminateUnion<EntityType> & Mutation<T>;
 
 export function isMutationOfType<T extends EntityType>(classType: { new (): T }) {
