6d9d249222204834e051834c1d7ed5c7fd5d982a
Maxence Dallongeville <maxence.dallongeville@360learning.com>
2020-11-05 20:26:52 +0100

    Remove namespace urgentWishForChildren

--- domain/actionSpace/urgentWishForChildren.ts
+++ domain/actionSpace/urgentWishForChildren.ts
@@ -1,15 +1,18 @@
 import { ActionSpace, ActionSpaceId, EntityMutation, EntityType, Game, PlayerId } from "../entity";
 import { bookActionSpace, giveBirthToDwarf } from "./utils";
 
-export namespace UrgentWishForChildren {
-    export function execute(game: Game, playerId: PlayerId): EntityMutation<EntityType>[] {
-        const actionSpaceId = ActionSpaceId.URGENT_WISH_FOR_CHILDREN;
-        const player = game.getPlayer(playerId);
-        const actionSpace = game.actionBoard.getActionSpace(actionSpaceId);
-        return [...bookActionSpace(actionSpace, player), ...giveBirthToDwarf(actionSpace, player)];
-    }
+export const UrgentWishForChildren = {
+    execute,
+    createActionSpace,
+};
 
-    export function createActionSpace() {
-        return new ActionSpace(ActionSpaceId.URGENT_WISH_FOR_CHILDREN, execute);
-    }
+function execute(game: Game, playerId: PlayerId): EntityMutation<EntityType>[] {
+    const actionSpaceId = ActionSpaceId.URGENT_WISH_FOR_CHILDREN;
+    const player = game.getPlayer(playerId);
+    const actionSpace = game.actionBoard.getActionSpace(actionSpaceId);
+    return [...bookActionSpace(actionSpace, player), ...giveBirthToDwarf(actionSpace, player)];
+}
+
+function createActionSpace() {
+    return new ActionSpace(ActionSpaceId.URGENT_WISH_FOR_CHILDREN, execute);
 }
