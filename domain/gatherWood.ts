996fc169a3c1a1d28db62bb2e684903c73f5bee7
Maxence Dallongeville <maxence.dallongeville@360learning.com>
2020-11-05 19:46:14 +0100

    Convert namespae GatherWood to object

--- domain/gatherWood.ts
+++ domain/gatherWood.ts
@@ -5,20 +5,23 @@ import { ActionSpace, ActionSpaceId } from "./entity/ActionSpace";
 import { Resources, ResourceType } from "./entity/Resources";
 import { bookActionSpace, takeResources } from "./actionUtils";
 
-export namespace GatherWood {
-    const REPLENISH_RESOURCES = {
-        ifEmpty: new Resources([[ResourceType.WOOD, 3]]),
-        ifNotEmpty: new Resources([[ResourceType.WOOD, 1]]),
-    };
+export const GatherWood = {
+    execute,
+    createActionSpace,
+};
 
-    export function execute(game: Game, playerId: PlayerId): EntityMutation<EntityType>[] {
-        const actionSpaceId = ActionSpaceId.GATHER_WOOD;
-        const player = game.getPlayer(playerId);
-        const actionSpace = game.actionBoard.getActionSpace(actionSpaceId);
-        return [...bookActionSpace(actionSpace, player), ...takeResources(actionSpace, player)];
-    }
+const REPLENISH_RESOURCES = {
+    ifEmpty: new Resources([[ResourceType.WOOD, 3]]),
+    ifNotEmpty: new Resources([[ResourceType.WOOD, 1]]),
+};
 
-    export function createActionSpace() {
-        return new ActionSpace(ActionSpaceId.GATHER_WOOD, execute, REPLENISH_RESOURCES);
-    }
+function execute(game: Game, playerId: PlayerId): EntityMutation<EntityType>[] {
+    const actionSpaceId = ActionSpaceId.GATHER_WOOD;
+    const player = game.getPlayer(playerId);
+    const actionSpace = game.actionBoard.getActionSpace(actionSpaceId);
+    return [...bookActionSpace(actionSpace, player), ...takeResources(actionSpace, player)];
+}
+
+function createActionSpace(): ActionSpace {
+    return new ActionSpace(ActionSpaceId.GATHER_WOOD, execute, REPLENISH_RESOURCES);
 }
